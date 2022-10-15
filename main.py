9b7685eb9a0016cbfd1602dbd6235ae9f61a2298
Rafay <Rafayberrypi@gmail.com>
2022-10-15 12:07:09 -0700

    no message here

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from drawable import drawable
 import pygame as pg
 pg.init()
-win = pg.display.set_mode(size=(640,700))
+WIDTH, HEIGHT = 640, 700
+win = pg.display.set_mode(size=(WIDTH, HEIGHT))
 
 sheet = pg.image.load("minesweeper-sprites.png")
-timer_numbers = [pg.surface.Surface((13, 23)) for x in range(10)]
+timer_numbers = [pg.surface.Surface((13, 23)) for x in range(11)]
 
-for x in range(10):
+for x in range(11):
     timer_numbers[x].blit(sheet, sheet.get_rect(x=-x * 14))
     timer_numbers[x] = drawable(timer_numbers[x], timer_numbers[x].get_rect(y=x * 24))
 #/////////////////////////////////////////////////////////////////
@@ -31,16 +32,36 @@ def draw(win, *objs):
         win.blit(obj.surface, obj.rect)
     pg.display.update()
 Main_grid = [[symbols[0].copy()for y in range(10)] for x in range(10)]
+XOFFSET, YOFFSET = WIDTH/2-80, HEIGHT/2-110
 I=0
 for row in Main_grid:
     J=0
     for spot in row:
-        spot.rect.x, spot.rect.y = J*(spot.rect.width+2), I*(spot.rect.height+2)
+        spot.rect.x, spot.rect.y = J*(spot.rect.width+2) + XOFFSET, I*(spot.rect.height+2) + YOFFSET
         J+=1
     I+=1
+flags_display = [timer_numbers[0].copy(), timer_numbers[1].copy(), timer_numbers[0].copy()]
+times_ran = 0
+for d in flags_display:
+    d.rect.x=XOFFSET + times_ran * (d.rect.width+1)
+    d.rect.y=YOFFSET -30
+    times_ran+=1
+
+flags_left = 10
+
+def translate_flags(flags_left, flags_display):
+    # Edit flags_display's pictures based on flags_left e.g. flags_left == 21, set f_d[0] = timer_numbers[0], f_d[1] = t_n[2], f_d[2] = t_n[1]
+    pass
+smile = faces[0]
+smile.rect.x = XOFFSET + 65
+smile.rect.y = YOFFSET -30
+
+
 play=True
 while play:
     for e in pg.event.get():
         if e.type==pg.QUIT:
             play=False
-    draw(win, *([item for row in Main_grid for item in row]))
+    draw(win, *([item for row in Main_grid for item in row]), *flags_display, smile)
+
+
