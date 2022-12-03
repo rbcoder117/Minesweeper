e8db133f07e8e7877c6e8df5d70a6c9524b7d32c
Rafay <Rafayberrypi@gmail.com>
2022-12-03 10:14:45 -0800

    idk

--- drawable.py
+++ drawable.py
@@ -2,5 +2,6 @@ class drawable:
     def __init__(self, surf, rect):
         self.rect = rect
         self.surface = surf
+        self.flag = False
     def copy(self):
         return drawable(self.surface.copy(), self.rect.copy())
